70867f5034dad627d25aa52931b1e0816dc26693
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2026-02-25 13:22:25 +0300

    chore: remove unused files

--- lib/features/users/data/datasources/user_data_source.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_technical_task_rightware_llc/features/users/data/models/user_model.dart';
-
-abstract class UserDataSource {
-  Future<List<DataModel>> getAllUsers(int pageId);
-  Future<DataModel> getUserDetails(int userId);
-}
